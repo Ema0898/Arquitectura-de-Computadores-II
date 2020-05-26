b8fb28b430fdf639c039f6c8dd2d237b49512dbf
ema0898 <aguisnchez@gmail.com>
2020-05-25 22:13:10 -0600

    Add for fixes

--- Proyecto1/src2/bus.py
+++ Proyecto1/src2/bus.py
@@ -5,9 +5,14 @@ class Bus:
   def __init__(self):
     self._memory = MainMemory()
 
-  def petition(self, owner, direction, data, request):
-    if request == "WM":
-      self._memory.setValue(owner, direction, data)
+  def petition(self, request):
+    #print("From bus got {}".format(request))
+    data = request.split(',')
+    if data[3] == "WM":
+      print("Writing data to memory")
+      self._memory.setValue(data[0], int(data[1]), int(data[2]))
+      return None
 
-    elif request == "RM":
-      return self._memory.getValue(direction)
+    elif data[3] == "RM":
+      #print("Returning data from memory to {}".format(data[0]))
+      return self._memory.getValue(int(data[1]))
